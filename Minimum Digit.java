107fb0134c3846f7b5c088a38460598cb870b758
Rohit Chatterjee <rohitchatterjee758@gmail.com>
2025-08-24 21:50:56 +0530

    Update Minimum Digit.java

--- Minimum Digit.java
+++ Minimum Digit.java
@@ -1,3 +1,42 @@
+/*Given a list L of N numbers, write a program to print the sum S, which is formed by adding the minimum digit from each number.  (TCS DIGITAL 2022)
+
+Input Format
+
+The first line of input contains N where N is size of the list L. The second line of input consists of N integers separated by a single white space.
+Constraints
+
+length of each number > 1
+0 < N < 106
+Output Format
+
+A single line of output contains the sum S.
+Sample Input 0
+
+4
+23 56 45 342
+Sample Output 0
+
+13
+Explanation 0
+
+Here N = 4, List L(N) = (23, 56, 45, 342).
+So after taking all the minimum digits of each number, We get 2, 5, 4, 2.
+After adding all the minimum digits, Sum S= 2+5+4+2 = 13 which is printed as the output.
+Sample Input 1
+
+5
+231 675 36 48 17
+Sample Output 1
+
+14
+Explanation 1
+
+Here N = 5, List L(N) = (231, 675, 36, 48, 17).
+
+So after taking all the minimum digits of each number, We get 1, 5, 3, 4, 1.
+
+After adding all the minimum digits, Sum S=1+5+3+4+1 = 14 which is printed as the output      */
+
 import java.util.*;
 
 public class calc {
